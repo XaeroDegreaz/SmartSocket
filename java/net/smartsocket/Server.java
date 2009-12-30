40547728c3ba18fef99cc31a58a8252c990efe37
Jerome Doby <xaerodegreaz@gmail.com>
2009-12-30 05:46:29 +0000

    Requires Swing Libraries and Beanshell Interpereter libraries == With this commit I have successfully brought the JAVA server on par with the PHP server. This server, however, supports multithreading natively.
    You can easliy write your own extensions just like you can for the PHP version of this server with the exception that your extensions are written in JAVA.
    
    Still to come:
    Demonstration extensions
    Advanced configurations

--- java/net/smartsocket/Server.java
+++ java/net/smartsocket/Server.java
@@ -3,78 +3,105 @@ package net.smartsocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import bsh.Interpreter;
+import java.lang.String;
+import java.lang.reflect.Method;
 
-public class Server implements Runnable{
+public class Server implements Runnable {
 
-    protected int          serverPort   = 8080;
+    protected int serverPort;
     protected ServerSocket serverSocket = null;
-    protected boolean      isStopped    = false;
-    protected Thread       runningThread= null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
     public String extension;
+    public Class _extension;
+    public Object _extensionInstance;
 
-    public Server(String extension, int port){
-        this.serverPort = port;
+    public Server(String extension, int port) {
+	this.serverPort = port;
 	this.extension = extension;
+
+	Interpreter i = new Interpreter();
+	try {
+	    //# Use BeanShell to grab the extension source.
+	    //i.source("./extensions/"+extension+"/"+extension+".java");
+	    //# use BeanShell to eval the source and create a Class object
+	    Class c = (Class) i.eval("source(\"./extensions/" + extension + "/" + extension + ".java\")");
+	    //# Create a new isntance of the extension's class
+	    this._extensionInstance = c.newInstance();
+	    this._extension = _extensionInstance.getClass();
+	} catch (Exception e) {
+	    Logger.log("Server", e.toString());
+	}
+
     }
 
-    public void run(){
-        synchronized(this){
-            this.runningThread = Thread.currentThread();
-        }
-        openServerSocket();
+    public void run() {
+	synchronized (this) {
+	    this.runningThread = Thread.currentThread();
+	}
+	openServerSocket();
 
-        while(! isStopped()){
+	while (!isStopped()) {
 
-            Socket clientSocket = null;
-            try {
-                clientSocket = this.serverSocket.accept();
-            } catch (IOException e) {
-                if(isStopped()) {
-                    System.out.println("Server Stopped.") ;
+	    Socket clientSocket = null;
+	    try {
+		clientSocket = this.serverSocket.accept();
+		
+		try {
+		    Class[] args = new Class[1];
+		    args[0] = Socket.class;
+		    Method m = _extension.getMethod("onConnect", args);
+		    m.invoke(_extensionInstance, clientSocket);
+		} catch (Exception e) {
+		    Logger.log(extension, "This extension does not have an onConnect method!");
+		}
+
+	    } catch (IOException e) {
+		if (isStopped()) {
+		    System.out.println("Server Stopped.");
 		    Logger.log("Server", "Server stopped.");
-                    return;
-                }
-		Logger.log("Server", "Error accepting client connection "+this.serverPort+" ["+e.toString()+"]");
-                throw new RuntimeException(
-                    "Error accepting client connection", e);
-            }
-	    Logger.log("Server", "Spawning new connection on port "+this.serverPort);
+		    return;
+		}
+		Logger.log("Server", "Error accepting client connection " + this.serverPort + " [" + e.toString() + "]");
+		throw new RuntimeException(
+			"Error accepting client connection", e);
+	    }
+	    Logger.log("Server", "Spawning new connection on port " + this.serverPort);
 
 	    try {
-		new Thread(new ThreadHandler(clientSocket)).start();
-	    }catch (Exception e) {
-		Logger.log("Server", "Error creating server for port "+this.serverPort+" ["+e.toString()+"]");
+		new Thread(new ThreadHandler(clientSocket, this)).start();
+	    } catch (Exception e) {
+		Logger.log("Server", "Error creating server for port " + this.serverPort + " [" + e.toString() + "]");
 		e.printStackTrace();
 	    }
-        }
-        System.out.println("Server Stopped.") ;
+	}
+	System.out.println("Server Stopped.");
 	Logger.log("Server", "Server stopped.");
     }
 
-
     private synchronized boolean isStopped() {
-        return this.isStopped;
+	return this.isStopped;
     }
 
-    public synchronized void stop(){
-        this.isStopped = true;
-        try {
-            this.serverSocket.close();
-        } catch (IOException e) {
-	    Logger.log("Server", "Error closing server "+this.serverPort+" ["+e.toString()+"]");
-            throw new RuntimeException("Error closing server", e);
-        }
+    public synchronized void stop() {
+	this.isStopped = true;
+	try {
+	    this.serverSocket.close();
+	} catch (IOException e) {
+	    Logger.log("Server", "Error closing server " + this.serverPort + " [" + e.toString() + "]");
+	    throw new RuntimeException("Error closing server", e);
+	}
     }
 
     private void openServerSocket() {
-        try {
-            this.serverSocket = new ServerSocket(this.serverPort);
-	   //Logger.log("Server", "Server initialized for "+this.extension);
-        } catch (IOException e) {
-	    Logger.log("Server", "Cannot open port "+this.serverPort+" ["+e.toString()+"]");
-            throw new RuntimeException("Cannot open port "+this.serverPort, e);
+	try {
+	    this.serverSocket = new ServerSocket(this.serverPort);
+	    //Logger.log("Server", "Server initialized for "+this.extension);
+	} catch (IOException e) {
+	    Logger.log("Server", "Cannot open port " + this.serverPort + " [" + e.toString() + "]");
+	    throw new RuntimeException("Cannot open port " + this.serverPort, e);
 
-        }
+	}
     }
-
 }
--- java/net/smartsocket/ThreadHandler.java
+++ java/net/smartsocket/ThreadHandler.java
@@ -11,61 +11,88 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.net.Socket;
 import java.util.Vector;
+import org.xml.sax.DocumentHandler;
+import org.json.simple.*;
 
 /**
  *
  * @author XaeroDegreaz
  */
-public class ThreadHandler implements Runnable {
+public class ThreadHandler extends Thread implements Runnable {
 
     static Vector handlers = new Vector(10);
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
-
+    public Server _server;
     //static SmartSocketJAVAApp _server;
 
-    public ThreadHandler(Socket socket) throws IOException {
-        this.socket = socket;
-        in = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(
-                new OutputStreamWriter(socket.getOutputStream()));
+    public ThreadHandler(Socket socket, Server server) throws IOException {
+	this.socket = socket;
+	_server = server;
+	in = new BufferedReader(
+		new InputStreamReader(socket.getInputStream()));
+	out = new PrintWriter(
+		new OutputStreamWriter(socket.getOutputStream()));
     }
 
     public void run() {
-        String line;
-        synchronized (handlers) {
-            handlers.addElement(this);
-        }
-        try {
-            while (!(line = in.readLine()).equalsIgnoreCase("/quit")) {
-                for (int i = 0; i < handlers.size(); i++) {
-                    synchronized (handlers) {
-                        ThreadHandler handler =
-                                (ThreadHandler) handlers.elementAt(i);
-                        handler.out.println(line + "\r");
-                        handler.out.flush();
-                    }
-                }
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
-            try {
-                in.close();
-                out.close();
-                socket.close();
-            } catch (IOException ioe) {
-            } finally {
-                synchronized (handlers) {
-                    handlers.removeElement(this);
-                }
+	String line;
+	synchronized (handlers) {
+	    handlers.addElement(this);
+	}
+	try {
+	    while (!(line = in.readLine()).equalsIgnoreCase("/quit")) {
+		/*for (int i = 0; i < handlers.size(); i++) {
+		synchronized (handlers) {
+		Logger.log("ThreadHandler-Run", line);
+		ThreadHandler handler =
+		(ThreadHandler) handlers.elementAt(i);
+		handler.out.println(line + "\r");
+		handler.out.flush();
+		}
+		}*/
+		try {
+		
+		//String s = "{\"c\":\"login\"}";
+		System.out.println("Receiving: "+line);
+		Object obj = JSONValue.parse(line);
+		JSONObject obj2 = (JSONObject) obj;
 
-            }
-        }
+		String command = obj2.get("c").toString();
+
+		Class[] args = new Class[2];
+		args[0] = Socket.class;
+		args[1] = Object.class;
+
+		Object params[] = {socket, obj};
+
+		Method m = _server._extension.getMethod(command, args);
+		m.invoke(_server._extensionInstance, params);
+
+		}catch(Exception e) {
+		    e.printStackTrace();
+		}
+
+	    }
+	} catch (IOException ioe) {
+	    ioe.printStackTrace();
+	} finally {
+	    try {
+		in.close();
+		out.close();
+		socket.close();
+	    } catch (IOException ioe) {
+	    } finally {
+		synchronized (handlers) {
+		    handlers.removeElement(this);
+		}
+
+	    }
+	}
     }
 }
 
