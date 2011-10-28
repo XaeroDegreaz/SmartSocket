7a62a9f750efb6cfdb09f16414f309e21c1cf3ba
Jerome Doby <xaerodegreaz@gmail.com>
2011-10-27 19:01:37 -0500

    * Removed Netbeans junk templates
    Signed-off-by: Jerome Doby <xaerodegreaz@gmail.com>

--- SmartSocket v2/src/net/smartsocket/client/SmartSocketClient.java
+++ SmartSocket v2/src/net/smartsocket/client/SmartSocketClient.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.smartsocket.client;
 
 import java.io.*;
--- SmartSocket v2/src/net/smartsocket/smartlobby/Room.java
+++ SmartSocket v2/src/net/smartsocket/smartlobby/Room.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.smartsocket.smartlobby;
 
 import com.google.gson.Gson;
